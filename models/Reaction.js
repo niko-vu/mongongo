67aab75ae945b712cc368abb864b18f21f256427
fearoffun <480dunce@gmail.com>
2023-08-29 00:06:04 -0700

    thought n reaction models and schemas updated

---
+++ models/Reaction.js
@@ -0,0 +1,34 @@
+const { Schema } = require('mongoose');
+
+const reactionSchema = new Schema(
+  {
+    reactionId: [
+        {
+          type: Schema.Types.ObjectId,
+        },
+      ],
+    reactionBody: {
+        type: String,
+        required: true,
+        maxLength: 280,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+const Reaction = Schema('reaction', reactionSchema);
+
+module.exports = Reaction;
---
+++ models/Thought.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 280,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    reactions: {
+        array: [],
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema
+  .virtual('reactionCount')
+  .get(function () {
+    return `${this.reactions.length}`;
+  });
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
